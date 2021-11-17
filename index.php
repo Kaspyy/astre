1f97e892eec0fae3979f62661624efbf8c61f9aa
Kaspyy <50270928+Kaspyy@users.noreply.github.com>
2021-11-17 15:44:01 +0100

    added php routing

--- index.php
+++ index.php
@@ -1,3 +1,11 @@
 <?php
 
-echo 'Hi there friend';
+require 'Routing.php';
+
+$path = trim($_SERVER['REQUEST_URI'], '/');
+$path = parse_url( $path, PHP_URL_PATH);
+
+Router::get('', 'DefaultController');
+Router::get('astre', 'DefaultController');
+
+Router::run($path);
