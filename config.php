f0316f357b4c68031b1f27a1ebb4ce6bf96f0d24
Kacper <kpawlak2000@gmail.com>
2021-12-15 17:37:44 +0100

    gitignore

--- config.php
+++ config.php
@@ -4,3 +4,4 @@ const USERNAME = 'uerzhhzchtuswc';
 const PASSWORD = 'eccc5181a81dfbdef986e91453b46e467fc95a3e144f073cda5043cd20b6b2de';
 const HOST = 'ec2-52-208-221-89.eu-west-1.compute.amazonaws.com';
 const DATABASE = 'd4pgao4ha48bnj';
+?>
